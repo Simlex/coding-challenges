2b99939ef176c042d89b718bb00ae6675e840c88
Simlex <simlexafol@gmail.com>
2024-01-26 00:51:40 +0100

    Add images to public folder

---
+++ public/images.ts
@@ -0,0 +1,16 @@
+// import logoWhite from "./images/logo_white.png";
+import profile_photo from "./images/profile_photo.jpg";
+import Marcus from "./images/Marcus.jpg";
+import Phillip from "./images/Phillip.jpg";
+import Jaydon from "./images/Jaydon.jpg";
+import Corey from "./images/Corey.jpg";
+import Cooper from "./images/Cooper.jpg";
+
+export default {
+  profile_photo,
+  Marcus,
+  Phillip,
+  Jaydon,
+  Corey,
+  Cooper,
+};
